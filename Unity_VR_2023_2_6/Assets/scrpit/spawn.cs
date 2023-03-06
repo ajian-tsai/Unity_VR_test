284952a0bb4a62ac6e5a734d50190d6f63b57ab6
ajian-tsai <chiufocal520@gmail.com>
2023-03-06 22:04:45 +0800

    SpawnBT_C#_soundBT_VS

---
+++ Unity_VR_2023_2_6/Assets/scrpit/spawn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    [Header("生成物件")]
+    public GameObject spawnBall;
+    [Header("生成位置")]
+    public Transform spawnPoint;
+
+    public void Spawn()
+    {
+        Instantiate(spawnBall, spawnPoint.position, spawnPoint.rotation);
+       //生成 ( 物件 、 物件位置 、 物件的旋轉值 )
+    }
+}
---
+++ Unity_VR_2023_2_6/Assets/scrpit/touch.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class touch : MonoBehaviour
+{
+    [Header("碰到畫布的圖案、顏料")]
+    public GameObject pictureOb;
+    [Header("碰到畫布的效果")]
+    public GameObject effectOb;
+    [Header("碰到畫布的音效")]
+    public AudioClip soundHit;
+}
